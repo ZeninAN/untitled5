96b192b2b28ee41afd0cf06aa040889e2005dd4f
Andrey <azenin010@gmail.com>
2022-01-18 17:40:35 +0400

    5.1

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,17 +1,17 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    int count = 0;
     int bottom = 100;
     int up = 200;
 
     public int square() {
-        for (int i = 10; i < 99; i++) {
+        int count = 0;
+        for (int i = 8; i < 100; i++) {
             if (i * i >= bottom && i * i <= up) {
                 count = count + 1;
             }
         }
-        System.out.println("Количество корней = "+count);
+        System.out.println("Количество корней = " + count);
         return count;
     }
 }
