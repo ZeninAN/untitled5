aadf2926811ffe7fa27d76ecfa7dec9abf07f8e1
Andrey <azenin010@gmail.com>
2022-01-24 19:03:40 +0400

    5.1

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,36 +1,8 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int square() {
+    public int square(int bottom, int up) {
         int count = 0;
-        int bottom = 100;
-        int up = 200;
-        for (int i = 10; i < 100; i++) {
-            if (i * i >= bottom && i * i <= up) {
-                count = count + 1;
-            }
-        }
-        System.out.println("Количество корней = " + count);
-        return count;
-    }
-
-    public int maxSquare() {
-        int count = 0;
-        int bottom = 100;
-        int up = 9801;
-        for (int i = 10; i < 100; i++) {
-            if (i * i >= bottom && i * i <= up) {
-                count = count + 1;
-            }
-        }
-        System.out.println("Количество корней = " + count);
-        return count;
-    }
-
-    public int notSquare() {
-        int count = 0;
-        int bottom = 5;
-        int up = 10;
         for (int i = 10; i < 100; i++) {
             if (i * i >= bottom && i * i <= up) {
                 count = count + 1;
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -8,24 +8,30 @@ class SQRServiceTest {
     @Test
     public void shouldSquare() {
         SQRService service = new SQRService();
+        int bottom = 100;
+        int up = 300;
         int expected = 5;
-        int actual = service.square();
+        int actual = service.square(bottom, up);
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldMaxSquare() {
         SQRService service = new SQRService();
+        int bottom = 100;
+        int up = 9801;
         int expected = 90;
-        int actual = service.maxSquare();
+        int actual = service.square(bottom, up);
         assertEquals(expected, actual);
     }
 
     @Test
     public void shouldNotSquare() {
         SQRService service = new SQRService();
+        int bottom = 3;
+        int up = 8;
         int expected = 0;
-        int actual = service.notSquare();
+        int actual = service.square(bottom, up);
         assertEquals(expected, actual);
     }
 }
